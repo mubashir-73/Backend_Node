d31d36669836d1920334e702d075d27704160088
mubashir-73 <mubashirsheriff7@gmail.com>
2024-11-17 12:38:44 +0530

    learn os and path modules

--- 3-modules.js
+++ 3-modules.js
@@ -1,7 +1,8 @@
 // Modules
-const names = require("./4-firstModule");
-
-console.log(names);
-//sayHI("susan");
-//sayHI(john);
-//sayHI(peter);
+const names = require("./4-firstModule.js");
+const greet = require("./5-utils.js");
+const data = require("./6-alternate-flavours.js");
+require("./7-mind-grenade.js");
+console.log(data);
+greet(names.john);
+greet(names.peter);
